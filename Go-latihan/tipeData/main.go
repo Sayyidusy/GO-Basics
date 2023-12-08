e3b69bd18b05495e7d889ea2a3d3ad8b69a7e89b
Sayyidusy syauqi Al Ghiffari <88390553+Sayyidusy@users.noreply.github.com>
2023-12-08 16:24:32 +0700

    latihan soal tipe data update

--- Go-latihan/tipeData/main.go
+++ Go-latihan/tipeData/main.go
@@ -1,65 +1,85 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
+	for {
+		fmt.Println("=== Menu ===")
+		fmt.Println("1. Luas dan Keliling Persegi Panjang")
+		fmt.Println("2. Luas Permukaan dan Volume Bola")
+		fmt.Println("3. Keluar")
 
-	//pilihan menu
-	fmt.Println("=== Menu ===")
-	fmt.Println("1. Luas dan Keliling Persegi Panjang")
-	fmt.Println("2. Luas Permukaan dan Volume Bola")
-	fmt.Println("3. Keluar")
+		var pilihan int
+		fmt.Print("Masukkan pilihan: ")
+		fmt.Scan(&pilihan)
 
-	var pilihan int
-	fmt.Print("Masukkan pilihan: ")
-	fmt.Scan(&pilihan)
+		switch pilihan {
+		case 1:
+			fmt.Println("=== luas dan keliling persegi panjang ===")
+			var panjang, lebar int
 
-	switch pilihan {
-	case 1:
-		fmt.Println("=== luas dan keliling persegi panjang ===")
-		var panjang, lebar int
+			fmt.Print("Masukkan panjang: ")
+			fmt.Scan(&panjang)
+			fmt.Print("Masukkan lebar: ")
+			fmt.Scan(&lebar)
 
-		fmt.Print("Masukkan panjang: ")
-		fmt.Scan(&panjang)
-		fmt.Print("Masukkan lebar: ")
-		fmt.Scan(&lebar)
+			//rumus luas persegi panjang
+			luas := panjang * lebar
+			//rumus keliling persegi panjang
+			keliling := 2 * (panjang + lebar)
 
-		//rumus luas persegi panjang
-		luas := panjang * lebar
-		//rumus keliling persegi panjang
-		keliling := 2 * (panjang + lebar)
+			//teks warna hijau
+			fmt.Println("\033[32m Luas persegi panjang =", luas, "\033[0m")
+			fmt.Print("\033[32m Keliling persegi panjang =", keliling, "\033[0m")
+			fmt.Println("")
 
-		fmt.Println("Luas persegi panjang =", luas)
-		fmt.Println("Keliling persegi panjang =", keliling)
-		fmt.Println("")
+		case 2:
+			fmt.Println("=== luas permukaan dan volume bola ===")
+			// rumus luas permukaan = 4 x π x r²
+			// rumus volume = 4/3 x π x r³
 
-	case 2:
-		fmt.Println("=== luas permukaan dan volume bola ===")
-		// rumus luas permukaan = 4 x π x r²
-		// rumus volume = 4/3 x π x r³
+			var jariJari float32
 
-		var jariJari float32
+			fmt.Println("masukan jari-jari: ")
+			fmt.Scan(&jariJari)
 
-		fmt.Println("masukan jari-jari: ")
-		fmt.Scan(&jariJari)
+			// luas permukaan bola
+			luasPermukaan := 4 * 3.14 * (jariJari * jariJari)
+			//volume bola
+			volume := 4 / 3 * 3.14 * (jariJari * jariJari * jariJari)
 
-		// luas permukaan bola
-		luasPermukaan := 4 * 3.14 * (jariJari * jariJari)
-		//volume bola
-		volume := 4 / 3 * 3.14 * (jariJari * jariJari * jariJari)
+			//teks warna hijau
+			fmt.Println("\033[32m Luas permukaan bola =", luasPermukaan, "\033[0m")
+			fmt.Println("\033[32m Volume bola =", volume, "\033[0m")
 
-		fmt.Println("luas permukaan bola =", luasPermukaan)
-		fmt.Println("volume bola =", volume)
+		case 3:
+			fmt.Println("Terima kasih")
 
-	case 3:
-		fmt.Println("=== keluar ===")
-		fmt.Println("Terima kasih telah menggunakan program ini")
+		//jika pilihan tidak ada
+		default:
+			fmt.Println("Pilihan tidak tersedia")
+		}
 
-	//jika pilihan tidak ada
-	default:
-		fmt.Println("Pilihan tidak tersedia")
+		fmt.Println("apakah anda ingin keluar? (Y/N)")
+		var keluar string
+		fmt.Scan(&keluar)
+
+		keluar = strings.ToUpper(keluar)
+		//kondisi jika ingin keluar
+		//jika user memasukan huruf besar atau kecil maka akan tetap dijalankan
+		if keluar == "Y" {
+			break
+		} else if keluar == "N" {
+			continue
+		} else {
+			fmt.Println("Pilihan tidak tersedia")
+		}
 	}
 
+	// soal latihan tipe data
 	// 1
 	// Buatlah program Go yang dapat menghitung
 	// luas dan keliling persegi panjang dengan panjang
